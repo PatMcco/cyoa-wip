9392a680505783bd6a58f4e60a60efe3a63778ae
PatMcCo <w0183220@nscc.ca>
2021-10-19 10:29:26 -0300

    day2.1

--- CYOA.py
+++ CYOA.py
@@ -12,44 +12,46 @@ def main():
     patience = 5
     health = 10
 ####Scenario Endings####
-    winScenario = ("Congratulations, you survived another trip to Wal-Mert\nwith your sanity intact. It could've been a lot worse.")
-    anxietyScenario = ("You suddenly feel flush and shakey, your vision narrows and you drop everything while making your way out of the store, you tell yourself you should've stayed home.")
-    patienceScenario = ("You've lost your patience. \nYou kick the next cart that's remotely close to your \nimmediate path out of here and tell multiple strangers to get F***ed as you \nstorm out flipping off the old Greeter lady.")
-    healthScenario = ("That was the last straw, you've managed to contract something\ndoctors don't have a name for and the side effects of it include\nhaving zero self-awareness, lack of compassion for other human beings\n and consistently seeking out the nearest manager.")
+    winScenario = "Congratulations, you survived another trip to Wal-Mert\nwith your sanity intact. It could've been a lot worse. Time to go home.\n\n"
+    anxietyScenario = "You suddenly feel flush and shakey, your vision narrows and you drop everything while making your way out of the store, you tell yourself you should've stayed home. Unfortunate but some days are just not great no matter what you do."
+    patienceScenario = "You've lost your patience. \nYou kick the next cart that's remotely close to your \nimmediate path out of here and tell multiple strangers to get F***ed as you \nstorm out flipping off the old Greeter lady. Won't be showing your face here for awhile..."
+    healthScenario = "That was the last straw, you've managed to contract something\ndoctors don't have a name for and the side effects of it include\nhaving zero self-awareness, lack of compassion for other human beings\n and consistently blocking aisles or laneways whilst going into a sort of comatose state. RIP"
+    karenScenario = "You feel your hair change shape. Suddenly everybody looks like they're a criminal who wants to hurt you. You can only ask for a manager, complain and threaten to call the police. You've become a Karen. Sad."
 
         
 ####Introduction####
     print("\nWelcome to the Anxious Person Wal-Mert Shopping Simulator\n")
     time.sleep(2)
     print("\nAnswer Y for Yes or N for No for each scenario.\n")
-    time.sleep(1)
+    time.sleep(2)
     print("\nYou see a van with reverse lights on close to the main entrance.\n")
-    time.sleep(1)
+    time.sleep(2)
 
-    check1 = input("Do you pull up close and wait for them to leave?: \n")
+    check1 = input("Do you pull up close and wait for them to leave?: \n\n")
     
     if str.lower(check1) == "y":
         print("\nThe person sits there for a solid 30 seconds without moving\n")
-        time.sleep(1.5)
+        time.sleep(2)
         print("\n---Your anxiety level escalates slightly...\n")
         anxiety = anxiety + 1
     elif str(check1) == "n":
         print("\nYou decide not to wait and move on to a further spot.\n")
+        time.sleep(2)
 
-    time.sleep(1)
+    time.sleep(2)
     print("\nYou park your car and head into the store.\nSomebody is blocking the entrance with\na cart and they're on their phone looking the other way.\n")
-    time.sleep(1)
+    time.sleep(2)
     check2 = input("\nYou have 3 choices: \n 1. Squeeze between the cart and door and ignore them. \n\n 2. Stand there until they realise they're blocking the way. \n\n 3. Say EXCUSE ME YOU'RE BLOCKING THE DOOR\n\n Choose 1, 2, or 3:\n ")
     if check2 == str(1):
-        time.sleep(1)
+        time.sleep(2)
         print("\nThey didn't even notice you and now there's a black stain from the cart on your pants\n\n---Your anxiety increases mildly and you're already regretting this trip.\n")
         anxiety = anxiety + 2
     elif check2 == str(2):
-        time.sleep(1)
+        time.sleep(2)
         print("\nAfter a solid 15 seconds they see you in the corner of their eye and move\nthe cart about a foot out of the way, not bothered at all \n\n---Your patience dwindles slightly.\n")
         patience = patience - 1
-    elif check2 == str(3):
-        time.sleep(1)
+    elif check2 == str(2):
+        time.sleep(2)
         print("\nThe person is visibly shocked and drops their phone, the gap opens and they don't realise what\nhappened until you're already well into the store.\n")
 
        ########level check#########
@@ -66,20 +68,18 @@ def main():
         time.sleep(10)
         sys.exit()
     ####Part 1 Item 1####
-    time.sleep(1)
+    time.sleep(2)
     print("\nYou make your way towards your first item.\n")
     time.sleep(2)
     print("\nYou get close to the aisle you need to go down.\n")
     time.sleep(2)
     print("\nSomeone is currently blocking it with their entire frikkin family and doesn't look to be moving soon.\n")
     time.sleep(2)
-    check3 = input("\nYou can either cut through the next aisle with the arrows facing the wrong way OR\n go all the way around.\n\nDo you cut through? \n")
-    time.sleep(1)
-
+    check3 = input("\nYou can either cut through the next aisle with the arrows facing the wrong way OR\ngo all the way around.\n\nDo you cut through? \n")
     if str.lower(check3) == "y":
-        time.sleep(1)
+        time.sleep(2)
         print("\nAs you cut through the aisle, a rather large woman looks at you.\nShe proceeds to turn her cart and gaze towards\nthe chocolate bars and shows no sign of moving.\n")
-        time.sleep(1)
+        time.sleep(2)
         print("\nYou can say Excuse me I just need to get by OR \ngo full tilt and throw your shoulder into her in an attempt to penetrate the wall of self rightousness\n")
         check3a = input("\nChoose 1 or 2: \n")
         if check3a == str(1):
@@ -95,7 +95,7 @@ def main():
             health = health - 2
             time.sleep(3)
             print("\n---Your patience is wearing thin and you suddenly feel off...\n")
-    elif check3 == str(3):
+    elif str.lower(check3) == "n":
         time.sleep(2)
         print("\nYou take the long way around through some coughing NPCs and pass some tempting impulse buys.\n\nYou manage to you arrive at your first item and grab it.\n")
         health = health - 1
@@ -119,13 +119,13 @@ def main():
     time.sleep(3)
     print("\nYour next item is just across the main racetrack. There are\n 3 other people in this aisle in a zig-zag pattern all with carts.\n")
     check4 = input("\nYou can get the item quicker by leaving your cart where it is,\nyou'll be quick afterall. Do you leave your cart?: \n")
-    time.sleep(1)
+    time.sleep(2)
     if str.lower(check4) == "y":
-        time.sleep(1)
+        time.sleep(2)
         print("\nYou make your way through the obstacle of old ladies complaining they've never seen bacon so expensive.\nAs you're coming to the end of the aisle you don't look both ways and get bumped by a small child.\nHe looks at you and then runs away.\nYou look down and see a wet spot where his face collided with your leg.\n You sigh, grab your item and make your way back to your cart.\n")
         health = health - 1
     else:
-        time.sleep(1)
+        time.sleep(2)
         print("\nYou take your cart and take the long way around again.\n On your way you hit up a hand sanitizer station.\n---You feel less gross. You grab your second item.")
     health = health + 1
 
@@ -147,9 +147,11 @@ def main():
     print("\nYou now have 2 of 5 items. Your next closest item is in the footwear aisle.\nAs you make your way there you come across a wet floor sign.\nSeems somebody had a pretty bad accident.\n\n")
     time.sleep(2)
     print("You don't want to push your cart through the mess,\nbut going against the floor arrows is a crime punishable by public humiliation these days.\n\n")
+    time.sleep(2)
     check5 = input("Do you say screw it and go around into the 'wrong way' aisle to avoid the mess?..\n\n" )
     if str.lower(check5) == "y":
         print("You maneuver the cart into the other lane, nobody seems to notice. Phew.\n\n")
+        time.sleep(2)
     elif str.lower(check5) == "n":
         print("You decide to uphold the Law of Arrows.\nYour carts wheels and your shoes are now covered in a biological hazard.\n\n")
         time.sleep(2)
@@ -157,6 +159,7 @@ def main():
         health = health - 3
         anxiety = anxiety + 1
         patience = patience - 1
+        check5 == False
         time.sleep(2.2)
 
         #######level check#########
@@ -174,11 +177,14 @@ def main():
         sys.exit()
 
     print("You arrive at the footwear section, you aren't exactly sure where the item you need is.\n\n")
+    time.sleep(2)
     check5  = input("You could ask someone for help or look yourself. Do you ask for help?\n\n")
+    time.sleep(2)
     if str.lower(check5) == "y":
         print("You find an employee who shows you to the item.\nYou thank him as you place your 3rd item in your cart.\n\n")
-        time.sleep(1.5)
-        print("The smell from the previous hazard is still lingering. Gross.\n\n")
+        time.sleep(2)
+        if check5 == False:
+            print("The smell from the previous hazard is still lingering. Gross.\n\n")
         health = health - 1
     elif str.lower(check5) == "n":
         print("You were too embarassed to ask for help. You look around for 20 minutes before finding your item.\n\n")
@@ -216,28 +222,31 @@ def main():
         check6a = input("Do you attempt to do their job for them?\n\n")
         if str.lower(check6a) == "y":
             print("You roll the stairs over, unlatching the 'Employee Use Only' sign.\nClimbing the stairs and grabbing your item.\nYou notice the handle is broken and instead grab another one.\nCouldn't see that from below. Sheesh.\n\n")
-            time.sleep(2)
+            time.sleep(3)
             print("As you place the item in your cart an employee walks by and makes eye contact.\nThey keep walking without saying a word.\n\n")
             anxiety = anxiety - 1
         elif str.lower(check6a) == "n":
             print("You continue searching for an employee. Another customer sees you and offers to help.\nThey're much taller and easily grab the item.\n\n")
+            time.sleep(3)
             print("The customer then starts ranting about how hard it is to find help when you need it.\nThey're face changes when they get closer to you.\nThey don't say anything but you know they noticed the smell from earlier. Oof\n\n")
             anxiety = anxiety + 1
+            time.sleep(4)
     elif check6 == str(2):
         print("Big strong person huh... You grab the item and the handle breaks as you pull it off the top shelf.\n\n")
-        time.sleep(1)
+        time.sleep(2)
         print("You manage to catch it before it lands on your head but in the process pull a muscle.\n\n")
-        time.sleep(1)
+        time.sleep(2)
         print("You've acquired your 4th item, but take some damage in the process. What is this some sort of game?\n\n")
         health = health - 3
         patience = patience - 1
+        time.sleep(2)
     elif check6 == str(3):
         print("You attempt to climb the shelving...\n\n")
         time.sleep(2)
         print("Your horrific smelling wet shoes slip on the edge of the rack and you fall.\n\n")
-        time.sleep(1)
-        print("An employee appears out of nowhere and grabs the item for you.\nAs they help you up they say:\n'We have you on camera doing this and driving through that spill earlier.\n If you complain to corporate you'll be barred from ever coming back.'\n\n")
         time.sleep(2)
+        print("An employee appears out of nowhere and grabs the item for you.\nAs they help you up they say:\n'We have you on camera doing this and driving through that spill earlier.\n If you complain to corporate you'll be barred from ever coming back.'\n\n")
+        time.sleep(4)
         print("Now completely embarassed you quickly lean on your cart and hobble away quietly.\n\n")
         health=health - 1
         anxiety = anxiety + 1
@@ -258,6 +267,44 @@ def main():
         sys.exit()
     
 ####PArt 7 Item 5####
+
+    print("You've made it this far and your last item is on the way to the register.\n\n")
+    time.sleep(3)
+    print("You grab it as you reach the register line-up.\n It was an .88 cent chocolate bar, never leave without it.\n\n")
+    time.sleep(2)
+    print("You now have all 5 items, the last choice you have to make is using the self-checkout\n or going to the one open register.\n\n")
+    time.sleep(2)
+    check7 = input("Do you use the self-checkout?\n\n")
+    if str.lower(check7) == "y":
+        time.sleep(2)
+        print("You roll your cart to the first open self-checkout.\n\n")
+        time.sleep(2)
+        print("As you begin unloading your items, you hear complaining\n\n")
+        time.sleep(2)
+        print("The complaining is about self-checkouts taking jobs away..of course.\n\n")
+        time.sleep(2)
+        print("The argument seems sound, unemployment rates ARE very high.\n\n")
+        time.sleep(2)
+        check7a = input("Do you chime in on the argument? It's a slipper slope...\n\n")
+        if str.lower(check7a) == "y":
+            print("'You're right and why am I paying full-price for doing all of the work?!?!\n\n")
+            time.sleep(2)
+            print("Your vision starts narrowing on a single point...\n\n")
+            time.sleep(2)
+            print("You feel the sudden urge to speak to someone in charge\n\n")
+            time.sleep(2)
+            print("You have forgotten completely why you came here\n\n")
+            time.sleep(2)
+            print("You try speaking but the only words that come out are...\n\n")
+            time.sleep(3)
+            print("LET ME SPEAK TO THE MANAGER\n\n")
+            time.sleep(2)
+            print(karenScenario)
+        elif str.lower(check7a) == "n":
+            print("Probably smart to keep your mouth shut.\n\nNothing good could come from arguing in Wal-Mert.")
+            time.sleep(2)
+
+
 
 
 main()
